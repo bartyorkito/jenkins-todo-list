7473ad7855584d406a7e0ff7351684be7b687a4b
bartyorkito <bartyorkito@gmail.com>
2023-11-09 16:40:46 -0300

    jenkins-alura

---
+++ scripts/jenkins.sh
@@ -0,0 +1,7 @@
+wget -O /usr/share/keyrings/jenkins-keyring.asc \
+	https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt-get update 
+apt-get install jenkins
